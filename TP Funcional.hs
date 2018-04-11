60f2dcdaad7c645a583be49e9367865e9386d3a0
Roy Eden <Roy.Ede@gmail.com>
2018-04-11 09:45:52 -0300

    Arreglos y correcciones menores

--- TP Funcional.hs
+++ TP Funcional.hs
@@ -4,27 +4,35 @@ import Data.List
 import Data.Maybe
 
 
-data Usuario = Usuario {nombre::String, billetera::Int} deriving(show,eq)
+data Usuario = Usuario {
+  nombre::String,
+  billetera::Int,
+  nivel::Int,
+  cantidadDeTransacciones::Int
+} deriving(Show,Eq)
 
-cambiarNombre nuevoNombre unUsuario = unUsuario {nombre = nuevoUsuario}
+
+cambiarNombre nuevoNombre unUsuario = unUsuario {nombre = nuevoNombre}
 cambiarBilletera nuevaBilletera unUsuario = unUsuario {billetera = nuevaBilletera}
-aumentarBilletera unaCantidad unUsuario = unUsuario {billetera = billetera + unaCantidad}
+aumentarBilletera unaCantidad unUsuario = unUsuario {billetera = billetera unUsuario + unaCantidad}
 disminuirBilletera unaCantidad = aumentarBilletera (unaCantidad * (-1))
+subirDeNivel unUsuario = unUsuario {nivel = nivel unUsuario + 1}
 
-pepe = Usuario "Jose"  10
-pepe2 = Usuario "Jose" 20  --Solo para pruebas--
-lucho = Usuario "Luciano"  2
+pepe = Usuario "Jose"  10 0
+pepe2 = cambiarBilletera 20 pepe 
+lucho = Usuario "Luciano"  2 0
 
 type Evento = Usuario -> Usuario
-deposito :: int -> Evento
-extraccion :: int -> Evento
+deposito :: Int -> Evento
+extraccion :: Int -> Evento
 upgrade :: Evento
 cierreDeCuenta :: Evento
 quedaIgual :: Evento
 
 deposito unaCantidad  = aumentarBilletera unaCantidad
-extraccion unaCantidad = min(0).disminuirBilletera unaCantidad
-upgrade unUsuario = aumentarBilletera (billetera unUsuario *0.2) unUsuario
+extraccion unaCantidad = (min(0) . disminuirBilletera) unaCantidad
+upgrade unUsuario | nivel unUsuario < 10 = (aumentarBilletera (billetera unUsuario * 0.2) . subirDeNivel) unUsuario
+                  | otherwise = quedaIgual unUsuario
 cierreDeCuenta = cambiarBilletera 0
 quedaIgual = aumentarBilletera 0
 tocoYmeVoy = (cierreDeCuenta.upgrade).deposito 15
@@ -35,15 +43,14 @@ type Transaccion = Evento
 transaccion1 :: Transaccion
 transaccion2 :: Transaccion
 
-transaccion1 unUsuario | unUsuario == lucho = cierreDeCuenta lucho  --Tengo dudas con esto, pero es una idea inicial--
-                       | otherwise quedaIgual unUsuario
+transaccion1 unUsuario | nombre unUsuario == "lucho" = cierreDeCuenta unUsuario
+                       | otherwise = quedaIgual unUsuario
 
-transaccion2 unUsuario | unUsuario == pepe = deposito 5 pepe
-                       | otherwise quedaIgual unUsuario
+transaccion2 unUsuario | nombre unUsuario == "pepe" = deposito 5 unUsuario
+                       | otherwise = quedaIgual unUsuario
 
 transaccion3 unUsuario | unUsuario == lucho = tocoYmeVoy lucho
-                       | otherwise quedaIgual unUsuario
+                       | otherwise = quedaIgual unUsuario
 
 transaccion4 unUsuario | unUsuario == lucho = ahorranteErrante lucho
-                       | otherwise quedaIgual unUsuario
---Repito mucha logica acá arriba, ver si se puede optimizar--
+                       | otherwise = quedaIgual unUsuario
