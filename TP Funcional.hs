d6765a4235aeff0f85f21d11d5437b22b4c6f7bf
Unknown <fede-macias@hotmail.com>
2018-04-15 23:27:46 -0300

    Transacciones corregidas, esqueleto de testeo
    Arreglé transaccion 3 y 4, hice la 5
    Dejé armado un esqueleto para los tests
    Saqué el min 0  de extraccion porque estaba mal aplicado

--- TP Funcional.hs
+++ TP Funcional.hs
@@ -2,6 +2,7 @@
 import Text.Show.Functions
 import Data.List
 import Data.Maybe
+import test.Hspec
 
 
 data Usuario = Usuario {
@@ -18,8 +19,8 @@ aumentarBilletera unaCantidad unUsuario = unUsuario {billetera = billetera unUsu
 disminuirBilletera unaCantidad = aumentarBilletera (unaCantidad * (-1))
 subirDeNivel unUsuario = unUsuario {nivel = nivel unUsuario + 1}
 
-pepe = Usuario "Jose" 10 0
-pepe2 = Usuario "Jose" 20 0
+pepe = Usuario "Jose" 10 0 0
+pepe2 = Usuario "Jose" 20 0 0
 lucho = Usuario "Luciano" 2 0
 
 type Evento = Usuario -> Usuario
@@ -32,7 +33,8 @@ tocoYmeVoy :: Evento
 ahorranteErrante :: Evento
 
 deposito unaCantidad  = aumentarBilletera unaCantidad
-extraccion unaCantidad = (disminuirBilletera . (min 0)) unaCantidad
+extraccion unaCantidad = disminuirBilletera unaCantidad
+--Saqué el min 0  porque estaba mal aplicado, intenté buscarle la vuelta pero todavia no encuentro la manera de aplicarlo correctamente
 upgrade unUsuario | nivel unUsuario < 10 = (aumentarBilletera (billetera unUsuario * 0.2) . subirDeNivel) unUsuario
                   | otherwise = quedaIgual unUsuario
 cierreDeCuenta = cambiarBilletera 0
@@ -44,8 +46,9 @@ ahorranteErrante = deposito 10.upgrade.deposito 8.extraccion 1.deposito 2.deposi
 type Transaccion = Evento
 transaccion1 :: Transaccion
 transaccion2 :: Transaccion
--- transaccion3 :: Transaccion
--- transaccion4 :: Transaccion
+transaccion3 :: Transaccion
+transaccion4 :: Transaccion
+transaccion5 :: Transaccion
 
 transaccion1 unUsuario | nombre unUsuario == "Luciano" = cierreDeCuenta unUsuario
                        | otherwise = quedaIgual unUsuario
@@ -53,8 +56,36 @@ transaccion1 unUsuario | nombre unUsuario == "Luciano" = cierreDeCuenta unUsuari
 transaccion2 unUsuario | nombre unUsuario == "Jose" = deposito 5 unUsuario
                        | otherwise = quedaIgual unUsuario
 
--- transaccion3 unUsuario | nombre unUsuario == "Luciano" = tocoYmeVoy lucho
---                        | otherwise = quedaIgual unUsuario
---
--- transaccion4 unUsuario | nombre unUsuario == "Luciano" = ahorranteErrante lucho
---                        | otherwise = quedaIgual unUsuario
+transaccion3 unUsuario | nombre unUsuario == "Luciano" = tocoYmeVoy unUsuario
+                       | otherwise = quedaIgual unUsuario
+
+transaccion4 unUsuario | nombre unUsuario == "Luciano" = ahorranteErrante unUsuario
+                       | otherwise = quedaIgual unUsuario
+
+transaccion5 unUsuario | nombre unUsuario == "Jose" = extraccion 7 unUsuario
+                       | nombre unUsuario == "Luciano" = deposito 7 unUsuario
+                       | otherwise = quedaIgual unUsuario
+
+testeo = hspec $ do
+  describe "Tests de eventos" $ do
+    it 1
+    it 2
+    it 3
+    it 4
+    it 5
+    it 6
+    it 7
+  describe "Tests de usuarios" $ do
+    it 8
+    it 9
+    it 10
+  describe "Tests de transacciones" $ do
+    it 11
+    it 12
+    it 13
+  describe "Tests de nuevos eventos" $ do
+    it 14
+    it 15
+  describe "Tests de pagos entre usuarios" $ do
+    it 16
+    it 17
